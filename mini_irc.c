17f831089cb51e80e8f6f6513cd0e96e53b85ce1
SamueleGalli <80041753+SamueleGalli@users.noreply.github.com>
2024-10-07 13:46:44 +0200

    Add files via upload

--- mini_irc.c
+++ mini_irc.c
@@ -1,29 +1,25 @@
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/select.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
 
 typedef struct client
 {
-    int id;
-    char msg[4242];
+    int id; //identificator of client
+    char msg[4242]; //size of message
 } t_client;
 
 typedef struct server
 {
-    int max;
-    int len;
-    int sockfd;
-    struct sockaddr_in servaddr;
-    struct sockaddr_in cli;
-    struct client c[1024];
-    socklen_t sock_len;
-    fd_set fds, fd_read, fd_write;
-    char write_b[424242];
-    char read_b[424242];
+    int max, sockfd; //max fd, socket of the fd of the server itself
+    struct sockaddr_in server, client; //hold server adress information, hold the client adress information
+    struct client c[1024]; //the number of client
+    socklen_t sock_len; //lenght of sockadress_in used when calling accept
+    fd_set fds, fd_read, fd_write; //fds (fds current fd) (fds_read fds for reading) (fd_write fd used to write)
+    char write_b[424242] ,read_b[424242]; //buffer for writing ,buffer for reading
 } t_server;
 
 void    print_error(char *s)
@@ -45,24 +41,30 @@ void    send_msg(int s_fd, t_server *s)
     }
 }
 
-int    accept_new_client(t_server *s, int next_id)
+int    accept_new_client(t_server *s, int ids)
 {
     //create a new socket
-    s->sock_len = sizeof(s->servaddr);
-    int c_socket = accept(s->sockfd, (struct sockaddr *)&s->servaddr, &s->sock_len);
+    s->sock_len = sizeof(s->server);
+    int c_socket = accept(s->sockfd, (struct sockaddr *)&s->server, &s->sock_len);
+
     //used to track the higest fd
     if (c_socket > s->max)
         s->max = c_socket;
-    //assigninf id to client
-    s->c[c_socket].id = next_id++;
+
+    //assigning id to client
+    s->c[c_socket].id = ids++;
+
     //clearing msg
-    bzero(s->c[c_socket].msg, strlen(s->c[c_socket].msg));
+    bzero(s->c[c_socket].msg, sizeof(s->c[c_socket].msg));
+
     //the socket is added to active socket
     FD_SET(c_socket, &s->fds);
+
     //sending welcome message to client
     sprintf(s->write_b, "server: client %d just arrived\n", s->c[c_socket].id);
     send_msg(c_socket, s);
-    return (next_id);
+
+    return (ids);
 }
 
 void    client_disconnect(t_server *s, int fd)
@@ -79,6 +81,7 @@ void    client_message(t_server *s, int fd, int read)
     {
         //adding data from buff to msg
         s->c[fd].msg[j] = s->read_b[i];
+
         //if i added \n then i replace it with the end of string
         if (s->c[fd].msg[j] == '\n')
         {
@@ -91,7 +94,7 @@ void    client_message(t_server *s, int fd, int read)
     }
 }
 
-void    handling_host(t_server *s, int next_id)
+void    handling_host(t_server *s, int ids)
 {
     while (1)
     {
@@ -107,12 +110,13 @@ void    handling_host(t_server *s, int next_id)
         for (int fd = 0; fd <= s->max; fd++)
         {
             if (FD_ISSET(fd, &s->fd_read) && fd == s->sockfd)
-                next_id = accept_new_client(s, next_id);
+                ids = accept_new_client(s, ids);
             //if a fd of a client try reading
             if (FD_ISSET(fd, &s->fd_read) && fd != s->sockfd)
             {
                 //read data
                 int read = recv(fd, s->read_b, sizeof(s->read_b), 0);
+
                 //if a client dissconect the message is sent to all other users
                 if (read <= 0)
                     client_disconnect(s, fd);
@@ -125,7 +129,7 @@ void    handling_host(t_server *s, int next_id)
 
 int main(int i, char **v)
 {
-    int next_id = 0;
+    int ids = 0; //update the id of client
     if (i != 2)
         print_error("Wrong number of arguments");
     t_server *s = malloc(sizeof(t_server));
@@ -139,15 +143,15 @@ int main(int i, char **v)
     FD_ZERO(&s->fds);
     //set the server socket to (s->fds)
     FD_SET(s->sockfd, &s->fds);
-    bzero(&s->servaddr, sizeof(s->servaddr));
+    bzero(&s->server, sizeof(s->server));
     bzero(s->c, sizeof(s->c));
-    s->servaddr.sin_family = AF_INET;
-    s->servaddr.sin_addr.s_addr = htonl(2130706433);
-    s->servaddr.sin_port = htons(atoi(v[1]));
-    if ((bind(s->sockfd, (const struct sockaddr *)&s->servaddr, sizeof(s->servaddr))) != 0)
+    s->server.sin_family = AF_INET;
+    s->server.sin_addr.s_addr = htonl(2130706433);
+    s->server.sin_port = htons(atoi(v[1]));
+    if ((bind(s->sockfd, (const struct sockaddr *)&s->server, sizeof(s->server))) != 0)
         print_error("Fatal error");
     if (listen(s->sockfd, 10) != 0)
         print_error("Fatal error");
-    s->len = sizeof(s->cli);
-    handling_host(s, next_id);
+    s->sock_len = sizeof(s->client);
+    handling_host(s, ids);
 }
